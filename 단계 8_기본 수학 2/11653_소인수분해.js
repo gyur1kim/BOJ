66682c6a1ec26efd6cc6cae17599cfe5ccec6e90
gyur1kim <qri1114@gmail.com>
2022-06-12 21:44:17 +0900

    220612

--- 단계 8_기본 수학 2/11653_소인수분해.js
+++ 단계 8_기본 수학 2/11653_소인수분해.js
@@ -1,10 +1,9 @@
-var input = +`72`;
+var input = +`1320`;
 var answer = '';
-var primeNum = [2];
-
 
 //아주 평범한 소수 구하기.. 근데 for문이 2개라 시간초과 뜬 듯
 /*
+var primeNum = [2];
 for(var i=2; i<=input; i++){
     for(var j=2; j<=Math.ceil(Math.sqrt(i)); j++){
         if(i%j===0){
@@ -15,40 +14,76 @@ for(var i=2; i<=input; i++){
         }
     }
 }
- */
+*/
 
-// //isPrime함수
-// function isPrime(n){
-//     if(n<2) return false;
-//     for(var i=2; i<=Math.floor(Math.sqrt(n)); i++){
-//         if(n%i===0){
-//             return false;
-//         }
-//         return true;
-//     }
-// }
-// //소수 리스트를 빨리 구해야된다
-// var primeList = new Array(input-1).fill().map((v, i)=>i+2);
-//
-//
-// primeList.filter(x => {
-//     if(isPrime(x)) return true;
-//     return false;
-// })
-// console.log(primeList);
+//isPrime함수
+/*
+function isPrime(n){
+    if(n<2) return false;
+    for(var i=2; i<=Math.floor(Math.sqrt(n)); i++){
+        if(n%i===0){
+            return false;
+        }
+        return true;
+    }
+}
+*/
+//소수 리스트를 빨리 구해야된다
+/*
+var primeList = new Array(input-1).fill().map((v, i)=>i+2);
+primeList.filter(x => {
+    if(isPrime(x)) return true;
+    return false;
+})
+console.log(primeList);
+*/
 
-// var k = 0;
-// function factorization(n){
-//     if(n<=1) return console.log(answer);
-//     if(n%primeNum[k] === 0){
-//         answer += primeNum[k] + '\n';
-//         n = n/primeNum[k];
-//         factorization(n);
-//     }
-//     else {
-//         k++;
-//         factorization(n);
-//     }
-// }
-//
-// factorization(input);
+//소인수분해하기
+/*
+var k = 0;
+function factorization(n){
+    if(n<=1) return console.log(answer);
+    if(n%primeNum[k] === 0){
+        answer += primeNum[k] + '\n';
+        n = n/primeNum[k];
+        factorization(n);
+    }
+    else {
+        k++;
+        factorization(n);
+    }
+}
+factorization(input);
+*/
+
+//이번엔 완벽하다 생각했는데............. 메모리초과 OMGㅜ
+//아무래도 리스트를 들어오는 크기만큼 만들어서, input이 10000000이면 메모리 제한을 넘어가나 보다.
+/*
+var primeList = new Array(input-1).fill().map((v, i)=>i+2);
+
+while(input > 1){
+    if(input%primeList[0]===0){
+        answer += primeList[0] + '\n';
+        input = input/primeList[0];
+    }
+    else{
+        var d = primeList[0];
+        primeList = primeList.filter((v)=> v%d !== 0);
+    }
+}
+console.log(answer);
+*/
+
+//그냥 2부터 열심히 나누는게 답인가보다..
+
+var k = 2;
+while(input > 1){
+    if(input%k === 0){
+        answer += k + '\n';
+        input /= k;
+    }
+    else{
+        k++;
+    }
+}
+console.log(answer);
---
+++ 단계 8_기본 수학 2/1929_소수 구하기.js
@@ -0,0 +1,92 @@
+var [m, n] = `9 100`.split(' ').map(Number);
+
+//에라토스의 체를 이용하면 왜! 시간초과가 될까?
+//isPrime 함수를 없애보자
+/*
+var arr = new Array(n-m+1).fill().map((v, i)=> i+m);
+
+function isPrime(n){
+    if(n < 2) return false;
+    else if(n === 2) return true;
+    else{
+        for(var i=2; i<Math.sqrt(n)+1; i++){
+            if(n%i!==0){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+*/
+
+//이 함수를 살짝 수정해보장
+/*
+function eratos(arr){
+    for(var j=2; j<Math.sqrt(n)+1; j++){
+        if(isPrime(j)){
+            console.log(j);
+            arr.forEach((v, i)=>{
+                if(v%j === 0 && j!==v){
+                    arr.splice(i, 1);
+                }
+            })
+            console.log(arr);
+        }
+    }
+}
+
+eratos(arr);
+console.log(arr.join('\n'));
+*/
+
+//새로 만든 에라토스의 체 좋았지만..그래도 시간초과야~!!ㅜ
+/*
+var newArr = new Array(n-1).fill().map((v, i)=> i+2);
+function newEratos(arr){
+    var i = 0;
+    while(i<=arr.length){
+        var d = arr[i];
+        arr.forEach((v, i)=>{
+            if(v%d === 0 && v!==d){
+                arr.splice(i, 1);
+            }
+        });
+        i++;
+    }
+    return arr;
+}
+
+var result = newEratos(newArr);
+console.log(result);
+for(i in result){
+    if(result[i] >= m){
+        result = result.splice(i);
+        break;
+    }
+}
+console.log(result);
+ */
+
+//그냥 일일히 나눠야하나보다.
+//일단 짝수로는 나누지 말아보자.
+var arr = [];
+if(m<=2) arr.push(2);
+if(m%2 === 0){
+    for(var i=m+1; i<=n; i+=2){
+        arr.push(i);
+    }
+}
+else{
+    for(var i=m; i<=n; i+=2){
+        if(i === 1) continue;
+        arr.push(i);
+    }
+}
+for(var j=3; j<Math.sqrt(n)+1; j+=2){
+    arr.forEach((v, i)=>{
+        if(v%j===0 && v!==j){
+            arr.splice(i, 1);
+        }
+    });
+}
+console.log(arr.join('\n'));
