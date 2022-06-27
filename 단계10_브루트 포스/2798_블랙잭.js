d6a90b4fb8febf320d8cb759d29454b1c76f81b7
gyur1kim <qri1114@gmail.com>
2022-06-27 23:52:46 +0900

    220627

---
+++ 단계10_브루트 포스/2798_블랙잭.js
@@ -0,0 +1,4 @@
+var [m, list] = `5 21
+5 6 7 8 9`.split('\n');
+m = m.slice(1);
+console.log(m, list)
--- 단계9_재귀/11729_하노이 탑 이동 순서.js
+++ 단계9_재귀/11729_하노이 탑 이동 순서.js
@@ -1,7 +1,16 @@
 var n = +`3`;
 var cnt = 0;
 var arr = new Array(n).fill(1);
-function hanoi(n){
-    v
-    arr[0]
+var output = '';
+function hanoi(numDisks, from, to, other){
+    //다 옮기면 끝내기
+    if(numDisks === 1) {
+        output += `${from} ${to}\n`;
+        return;
+    }
+    hanoi(numDisks-1, from, other, to); //원래 있던 것을 일단 보조기둥으로 옮기기
+    hanoi(numDisks-1, other, to, from); //보조기둥에 있던 것을 목적지로 옮기기
 }
+hanoi(n, 1, 3, 2);
+console.log(2**n -1);
+console.log(output);
