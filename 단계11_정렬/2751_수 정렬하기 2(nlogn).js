41987ad92df7b68e3739f3ad076f63892d7fca0f
gyur1kim <qri1114@gmail.com>
2022-07-08 01:56:58 +0900

    220708

--- 단계11_정렬/2751_수 정렬하기 2(nlogn).js
+++ 단계11_정렬/2751_수 정렬하기 2(nlogn).js
@@ -1,5 +1,4 @@
 //병합 정렬, 퀵 정렬, 힙 정렬
-
 var [n, ...input] = `9
 2
 4
@@ -12,6 +11,7 @@ var [n, ...input] = `9
 9`.split('\n');
 input=input.map(Number);
 console.log(input);
+
 //병합 정렬
 /*
 1. 리스트의 길이가 0 또는 1이면 이미 정렬된 것으로 본다. 그렇지 않은 경우에는
@@ -19,36 +19,25 @@ console.log(input);
 3. 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다.
 4. 두 부분 리스트를 다시 하나의 정렬된 리스트로 합병한다.
  */
-//1. 나누는 함수
+//1. 배열을 반씩 나누는 함수
 function mergeSort(list){
-    if(list.length === 1){      //배열의 길이가 1이 되면 끝내기
-        return list;
-    }
+    if(list.length === 1) return list;          //배열의 길이가 1이 되면 끝낸다
     var half = Math.floor(list.length/2);     //중간값
     var left = list.slice(0, half);  //왼쪽 배열
     var right = list.slice(half);   //오른쪽 배열
-    console.log(left);
-    console.log(right);
-    merge(mergeSort(left), mergeSort(right));       //배열의 길이가 1이 되면 merge시작.
+    return merge(mergeSort(left), mergeSort(right));       //배열의 길이가 1이 되면 merge시작, 이 값을 계속 return해서 최종적으로 합친 것이 병합 정렬
 }
-
-//2. 합치는 함수
+//2. 나눈 배열을 다시 순서에 맞게 합치는 함수
 function merge(left, right){
     var answer = [];
-    console.log(left.length);
     while(left.length && right.length){
-        console.log(left);
-        console.log(right);
-        if(left[0]>=right[0]){
-            answer.push(right.shift());
-        }
-        else{
-            answer.push(left.shift());
-        }
+        if(left[0]>=right[0]) answer.push(right.shift());
+        else answer.push(left.shift());
     }
-    console.log(answer);
     return [...answer, ...left, ...right];
 }
+
 var result = mergeSort(input);
-console.log(input);
-console.log(result);
+console.log(result.join('\n'));
+
+//퀵 정렬
