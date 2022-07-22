51bd5086b283a954035dc5dbe2023c06647b6431
gyur1kim <qri1114@gmail.com>
2022-07-22 23:52:44 +0900

    220722

--- 단계12_집합과 맵/10815_숫자 카드.js
+++ 단계12_집합과 맵/10815_숫자 카드.js
@@ -2,11 +2,11 @@ let [n, myCard, m, compareCard] = `5
 6 3 2 10 -10
 8
 10 9 -5 2 3 4 5 -10`.split('\n')
-myCard = myCard.split(' ').map(Number);
+myCard = myCard.split(' ');
 compareCard = compareCard.split(' ');
 
 let myCard_object = {}
-myCard.forEach((v, i)=>{
+myCard.forEach((v)=>{
     myCard_object[v] = 1;
 });
 let answer = ''
@@ -16,3 +16,13 @@ compareCard.forEach((v)=>{
 console.log(answer)
 
 // 숏코딩 - map을 이용해 풀었다.
+let myCard_map = new Map();
+myCard.map(v=> myCard_map.set(v, 1));
+let answer2 = compareCard.map(v => myCard_map.get(v) || 0);
+console.log(answer2.join(' '))
+
+//object와 map을 쓰고 나서 알게된 점
+/*
+object보다 map 객체가 더 빠르다.
+forEach보다 map이 더 빠르다.
+ */
