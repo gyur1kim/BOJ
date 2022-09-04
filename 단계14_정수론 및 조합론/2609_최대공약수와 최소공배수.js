119e698f3087c16a917563c86029a6ced29b24c6
gyur1kim <qri1114@gmail.com>
2022-09-04 14:57:03 +0900

    220904_최소공배수

--- 단계14_정수론 및 조합론/2609_최대공약수와 최소공배수.js
+++ 단계14_정수론 및 조합론/2609_최대공약수와 최소공배수.js
@@ -1,27 +1,24 @@
-let [a, b] = `121 132`.trim().split(' ').map(Number);
+let [a, b] = `24 5`.trim().split(' ').map(Number);
 /*
 유클리드 알고리즘!!
-큰 값에서 작은 값으로 나눈 나머지가 0이면? b가 최대공약수가 된다.
-큰 값에서 작은 값으로 나눈 나머지가 n이면(n!=0)? b=n을 한 뒤 다시 나눈다... 무한 반복
+큰 값에서 작은 값으로 나눈 나머지가 0이면? 작은 값이 최대공약수가 된다.
+큰 값에서 작은 값으로 나눈 나머지가 0이 아니면? 작은 값을 나머지로 나눈다... 무한 반복
  */
-
 if (a < b){
     [a, b] = [b, a];
 }
+let A = a;
 let B = b;
-
-let GCD = 0   // 최대 공약수 gcd
-while(true){
-    let n = a%b;
-    if(n === 0){
-        GCD = b;
+let GCD = 1;
+while(B!==0){
+    let r = A%B;
+    if(r===0){
+        GCD = B;
         break;
     }
-    else{
-        b = n;
-    }
+    A = B;
+    B = r;
 }
-// console.log(GCD);
-let LCM = a * (B/GCD);  // 최소공배수 LCM
 console.log(GCD);
+let LCM = a * (b/GCD);  // 최소공배수 LCM
 console.log(LCM);
