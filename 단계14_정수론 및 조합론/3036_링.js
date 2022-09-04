f230a20b7ed4c6dc7b94182faae601797b461940
gyur1kim <qri1114@gmail.com>
2022-09-04 15:08:29 +0900

    220904_링(최대공약수)

--- 단계14_정수론 및 조합론/3036_링.js
+++ 단계14_정수론 및 조합론/3036_링.js
@@ -1,11 +1,19 @@
-let input = `3
-8 4 2`.split(/\s+/).map(Number);
+let input = `4
+300 1 1 300`.split(/\s+/).map(Number);
 
 let n = input[0];
-let first = input[1];
+let base = input[1];
 let rest = input.splice(2);
 
 let ans = ''
 for(let i=0; i<n-1; i++){
-
+    let a = base;
+    let b = rest[i];
+    if (a<b){
+        [a, b] = [b, a];
+    }
+    const gcd = (a, b) => a%b===0? b : gcd(b, a%b);
+    let res = gcd(a, b);
+    ans += (base/res) + '/' + (rest[i]/res) + '\n';
 }
+console.log(ans);
