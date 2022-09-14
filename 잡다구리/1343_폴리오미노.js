c23cb7e6a21b8824c70361e49317f4c528a50591
gyur1kim <qri1114@gmail.com>
2022-09-14 22:53:26 +0900

    220914

--- 잡다구리/1343_폴리오미노.js
+++ 잡다구리/1343_폴리오미노.js
@@ -24,3 +24,11 @@ for(let i=0; i<board.length; i++){
 }
 if(isDone) console.log(board.slice(0, board.length-1).join(''));
 
+
+// 정규표현식을 알면 풀 수 있는 방안도 있다.
+let board2 = `XXXXXX`;
+// 글로벌하게 바꾸겠다는 뜻이다. 그냥 문자열은 1개만 바뀌므로 정규표현식을 이용해 모든 글자를 바꾼다.
+// replace는 문자열을 바꾸는 메서드
+board2 = board2.replace(/XXXX/g, 'AAAA');
+board2 = board2.replace(/XX/g, 'BB');
+console.log(board2.split('').includes('X')? -1: board2);
