5d7224acd08afcd16a8fd4fb9380d75d6c6ea301
gyur1kim <qri1114@gmail.com>
2023-03-28 00:50:11 +0900

    16173_풀긴했는데 느림(184ms)

--- DFS BFS/16173_점프왕 쩰리(small).js
+++ DFS BFS/16173_점프왕 쩰리(small).js
@@ -2,32 +2,40 @@ let [n, ...zone] = `3
 1 1 10
 1 5 1
 2 2 -1`.split('\n');
-
 zone = zone.map(x=>x.split(' ').map(Number));
-console.log(zone);
+let res = "Hing";
 
-solution(0, 0)
-
-const jellyPosition = []
 function solution (x, y) {
-  // 현재 젤리 위치
-  jellyPosition.push([x, y])
-
   // 젤리가 움직일 수 있는 크기
-  let goSize = zone[x][y];
+  let go = zone[x][y];
 
-  // 아래로 가는 경우
-  let jellyGoDown = [x + goSize, y];
-  // 오른쪽으로 가는 경우
-  let jellyGoRight = [x, y + goSize];
+  // 한 번 왔던 곳은 다시 탐색하지 않도록 -100로 바꿔 다녀갔음을 표시함
+  zone[x][y] = -100
 
-  if (jellyGoDown === [n-1, n-1] || jellyGoRight === [n-1, n-1]) {
-    res = "HaruHaru"
-    return res;
+  // 너무 숫자가 크면 굳이 안 봐도 되니까 땡
+  if (go >= n) return;
+
+  if (res === "Hing") {
+    // 오른쪽으로 가는 거 검사
+    if (y+go < n) {
+      if (zone[x][y+go] === -1) {
+        res = "HaruHaru";
+        return;
+      }
+      else if (zone[x][y+go] !== -100) {
+        solution(x, y + go)
+      }
+    }
+    // 아래로 가는 거 검사하기
+    if (x+go < n) {
+      if (zone[x+go][y] === -1) {
+        res = "HaruHaru";
+      }
+      else if (zone[x+go][y] !== -100) {
+        solution(x + go, y)
+      }
+    }
   }
-
-  if (jellyGoDown[0] < n && jellyGoDown[1] < n) {
-    res = solution([...jellyGoDown]);
-  }
-
 }
+solution(0, 0);
+console.log(res);
