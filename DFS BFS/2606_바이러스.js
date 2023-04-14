b8f0366b7154449f3c6f7a3025d700c660a09d72
gyur1kim <qri1114@gmail.com>
2023-04-14 23:40:29 +0900

    128ms, 9452KB

--- DFS BFS/2606_바이러스.js
+++ DFS BFS/2606_바이러스.js
@@ -9,35 +9,31 @@ let [computerNum, connectedNum, ...computers] = `7
 
 /** 연결된 컴퓨터들을 나타내기 */
 const computerArr = Array.from(Array(+computerNum+1), ()=>Array().fill([]));
-console.log(computerArr);
+// Array를 만드는 또 다른 방법
+// let graph = [...new Array(n+1)].map(() => []);
 for(let i=0; i<+connectedNum; i++) {
   let [v, w] = computers[i].split(' ').map(Number);
   computerArr[v].push(w);
   computerArr[w].push(v);
 }
-console.log(computerArr);
-
-
 
 function BFS () {
   const visited = Array(+computerNum+1).fill(false);
   const queue = [1];     // 1번 컴퓨터보터 볼게요
+  visited[1] = true;
   let head = 0;
   let count = 0;
 
   while (head < queue.length) {
     let computer = queue[head++];
-    visited[computer] = true;
-
-
     for (let neighbor of computerArr[computer]) {
       if (!visited[neighbor]) {
+        visited[neighbor] = true;
         count++;
         queue.push(neighbor);
       }
     }
   }
-
   return count;
 }
 
