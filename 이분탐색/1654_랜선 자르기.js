485c10f09d231a626f1725e6ff183f6306530040
gyur1kim <qri1114@gmail.com>
2023-04-27 22:47:01 +0900

    :recycle: S2-196ms
    - for.. of 문에서 for문으로 변경했더니 시간이 20ms 단축했다

--- 이분탐색/1654_랜선 자르기.js
+++ 이분탐색/1654_랜선 자르기.js
@@ -11,18 +11,21 @@ function solution () {
     let tmpLength = Math.floor((max+min)/2);
     let tmpCnt = 0;
 
-    for (let LAN of LANs) {
-      tmpCnt += Math.floor(LAN / tmpLength);
+    // for (let LAN of LANs) {
+    //   tmpCnt += Math.floor(LAN / tmpLength);
+    // }
+    for (let i=0; i<K; i++) {
+      tmpCnt += Math.floor(LANs[i] / tmpLength);
     }
 
-    if (tmpCnt === N) {            // 내가 원하는 조각 수지만.. 그래도 더 늘려서 잘라보자
+    if (tmpCnt >= N) {            // 내가 원하는 조각 수지만.. 그래도 더 늘려서 잘라보자
       LANLength = tmpLength;
       min = tmpLength + 1;
     }
-    else if (tmpCnt > N) {    // 내가 원한 조각수보다 많이 잘린 것 -> 크기를 늘리자
-      LANLength = tmpLength;
-      min = tmpLength + 1;
-    }
+    // else if (tmpCnt > N) {    // 내가 원한 조각수보다 많이 잘린 것 -> 크기를 늘리자
+    //   LANLength = tmpLength;
+    //   min = tmpLength + 1;
+    // }
     else {   // 내가 원한 조각수보다 적게 잘린 것 -> 크기를 줄이자
       max = tmpLength - 1;
     }
