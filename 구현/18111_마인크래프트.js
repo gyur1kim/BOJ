672284deb39645fc5174ca6d3064d0692159f9c7
gyur1kim <qri1114@gmail.com>
2023-05-05 23:52:55 +0900

    18111_minecraft

--- 구현/18111_마인크래프트.js
+++ 구현/18111_마인크래프트.js
@@ -6,3 +6,30 @@
  * 2. 평균 높이를 기준으로 다시 땅을 돌아보면서 높으면 깎고 낮으면 심어본다???
  *    그러면서 구하면 되는거 아닌가
  */
+
+function solution (N, M, B, ground) {
+  const sortedGround = ground.sort((a, b) => a - b);
+
+  /*
+   * 일단 평균 높이부터 구해보자...??
+   */
+
+  let avgHeight = Math.round(sortedGround.reduce((acc, cur) => acc + cur, 0)/(N*M))
+
+  for (let h=sortedGround[0]; h<=sortedGround[N*M-1]; h++) {
+
+  }
+
+
+
+
+  return avgHeight;
+
+}
+
+let [N, M, B, ...ground] = `3 4 1
+64 64 64 64
+64 64 64 64
+64 64 64 63`.split(/\s+/).map(Number);
+
+console.log(solution(N, M, B, ground))
