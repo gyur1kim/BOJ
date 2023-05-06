6fe168233baef75cbd644b27c7ab7e922282ed9c
gyur1kim <qri1114@gmail.com>
2023-05-06 18:14:17 +0900

    380ms_52036KB
    - 다른 사람들 코드 보며 리팩토링하기

--- 구현/18111_마인크래프트.js
+++ 구현/18111_마인크래프트.js
@@ -2,34 +2,58 @@
  * 인벤토리의 블록으로 땅을 메우기 - 1초
  * 블록을 제거하여 인벤토리에 넣기 - 2초
  *
- * 1. 땅을 다 돌면서 평균 높이를 구해본다?
- * 2. 평균 높이를 기준으로 다시 땅을 돌아보면서 높으면 깎고 낮으면 심어본다???
- *    그러면서 구하면 되는거 아닌가
+ * 지금 내가 이해하기로는
+ * - 모든 높이 (0부터 256까지)에 대해서 구해본다 => 나는 최소 크기부터 최대 크기로 할 생각임.
+ * - 이 때, 높이 기준으로 파내야 하는 영역과 채워야하는 영역의 크기를 각각 구해둔다(누적이겠죠?)
+ * - 이를 기준으로 빈 칸보다 인벤토리가 크면 인벤토리로 채우고, 그게 아니면 인벤토리 + 나머지 영역 뽑아다 심자.
  */
 
 function solution (N, M, B, ground) {
   const sortedGround = ground.sort((a, b) => a - b);
+  let wholeBlocks = sortedGround.reduce((acc, cur) => acc + cur, 0);
 
-  /*
-   * 일단 평균 높이부터 구해보자...??
-   */
+  let baseBlocks = 0;
+  let restBlocks = 0;
+  let empties = 0;
 
-  let avgHeight = Math.round(sortedGround.reduce((acc, cur) => acc + cur, 0)/(N*M))
+  let maxHeight = 0;
+  let time = 500 * 500 * 256 * 3;
 
-  for (let h=sortedGround[0]; h<=sortedGround[N*M-1]; h++) {
+  let min = sortedGround[0];
+  let max = sortedGround[N * M - 1];
+  let idx = 0;
+  let prevIdx = 0;
 
+  for (let h = min; h <= max; h++) {
+    let tmpHeight = h;
+    prevIdx = idx;
+    while (idx < N * M && sortedGround[idx] < h) idx++;
+    empties += idx;
+    baseBlocks = N * M * h  - empties;
+    restBlocks = wholeBlocks - baseBlocks;
+
+    let tmpTime = 0;
+    if ( empties <= B + restBlocks ) {   // 빈 공간을 채울 수 있는 경우, 깎아서 쓰든지 인벤토리에서 갖다 쓰든지...
+      tmpTime += empties;
+      tmpTime += restBlocks * 2;
+    }
+    else {                               // 빈 공간을 채울 수 없는 경우.. 이럴땐 어떡해
+      tmpTime = time;
+      tmpHeight = maxHeight;
+    }
+
+
+    if (tmpTime <= time) {
+      maxHeight = tmpHeight;
+      time = tmpTime;
+    }
   }
 
-
-
-
-  return avgHeight;
-
+  return `${time} ${maxHeight}`;
 }
 
-let [N, M, B, ...ground] = `3 4 1
-64 64 64 64
-64 64 64 64
-64 64 64 63`.split(/\s+/).map(Number);
+let [N, M, B, ...ground] = `2 2 68
+120 90
+250 170`.split(/\s+/).map(Number);
 
 console.log(solution(N, M, B, ground))
