dede498aeb9249cb778822da1efd1395207f6539
gyur1kim <qri1114@gmail.com>
2023-05-08 23:56:10 +0900

    시간 +68ms, 메모리 +828KB
    - 다른 사람들 코드를 확인해야겠다..

--- 구현/18111_마인크래프트.js
+++ 구현/18111_마인크래프트.js
@@ -8,7 +8,10 @@
  * - 이를 기준으로 빈 칸보다 인벤토리가 크면 인벤토리로 채우고, 그게 아니면 인벤토리 + 나머지 영역 뽑아다 심자.
  */
 
-function solution (N, M, B, ground) {
+
+// 이 방식은 시간도 오래 걸리고 메모리도 많이 차지했다. 정렬을 하지 말까..
+// 52036KB, 380ms
+function solution1 (N, M, B, ground) {
   const sortedGround = ground.sort((a, b) => a - b);
   let wholeBlocks = sortedGround.reduce((acc, cur) => acc + cur, 0);
 
@@ -42,6 +45,50 @@ function solution (N, M, B, ground) {
       tmpHeight = maxHeight;
     }
 
+
+    if (tmpTime <= time) {
+      maxHeight = tmpHeight;
+      time = tmpTime;
+    }
+  }
+
+  return `${time} ${maxHeight}`;
+}
+
+// 더 느려짐... 52864KB, 448ms
+function solution (N, M, B, ground) {
+  let maxHeight = 0;
+  let time = 500 * 500 * 256 * 3;
+
+  let min = Math.min(...ground)   // 최저 높이
+  let max = Math.max(...ground)   // 최고 높이
+
+  for (let h = min; h <= max; h++) {
+    let tmpHeight = h;
+    let tmpTime = 0;
+
+    let restBlocks = 0;
+    let empties = 0;
+
+    for (let i=0; i<ground.length; i++) {
+      if (ground[i] <= tmpHeight) {    // 현재 기준이되는 높이보다 땅이 낮으면?
+        empties += tmpHeight - ground[i];
+      }
+      else {    // 현재 기준이 되는 높이보다 땅이 높으면?
+        restBlocks += ground[i] - tmpHeight;
+      }
+    }
+
+    console.log(restBlocks);
+    console.log(empties);
+
+    if ( empties <= B + restBlocks ) {   // 빈 공간을 채울 수 있는 경우, 깎아서 쓰든지 인벤토리에서 갖다 쓰든지...
+      tmpTime += empties + restBlocks * 2;
+    }
+    else {                               // 빈 공간을 채울 수 없는 경우.. 이럴땐 어떡해
+      tmpTime = time;
+      tmpHeight = maxHeight;
+    }
 
     if (tmpTime <= time) {
       maxHeight = tmpHeight;
