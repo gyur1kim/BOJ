14a33be2e475b758869b32a2bca88dece509d139
gyur1kim <qri1114@gmail.com>
2023-05-13 15:46:10 +0900

    230513_9620KB, 116ms

---
+++ DP/9461_파도반 수열.js
@@ -0,0 +1,25 @@
+/*
+ * 1, 1, 1, 2, 2, 3, 4, 5, 7, 9
+ * 바로 자신 전의 삼각형, 그리고 다섯 번째 전의 삼각형의 변의 길이를 구한 것과 같다.
+ * 빙글 빙글 돌아서.. 그런듯
+ */
+
+const solution = (T, N) => {
+  const memo = [0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9];
+  // length는 내가 구하고자 하는 N보다 항상 +1 크다. 앞에 idx=0인 경우를 넣었거든
+  let ans = [];
+  for (let t=0; t<T; t++) {
+    let n = N[t];
+    while (memo.length <= n) {
+      memo.push(memo[memo.length-1] + memo[memo.length-5]);
+    }
+    ans.push(memo[n])
+  }
+
+  return ans.join('\n');
+}
+
+let [T, ...N] = `2
+6
+12`.split(/\s+/).map(Number);
+console.log(solution(T, N))
