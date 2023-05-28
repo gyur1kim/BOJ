f2737ee6d902aeacfa8b99e894ecac7bb54c3d5f
gyur1kim <qri1114@gmail.com>
2023-05-28 23:54:28 +0900

    :memo: README 추가
    - 1463문제 README 추가

--- DP/1463_1로 만들기.js
+++ DP/1463_1로 만들기.js
@@ -1,4 +1,4 @@
-let dp = [0, 1, 1, 1];
+let dp = [0, 0, 1, 1];
 
 // x를 구하려면?
 //   1. 이전 값에서 +1을 한다
@@ -7,19 +7,13 @@ let dp = [0, 1, 1, 1];
 
 let input = 10
 
-
 for (let i=4; i<=input; i++) {
   dp.push(dp[i-1] + 1);
-
-
   if (i % 2 === 0) {
     dp[i] = Math.min(dp[i], dp[i/2] + 1);
   }
   if (i % 3 === 0) {
     dp[i] = Math.min(dp[i], dp[i/3] + 1);
   }
-
-  console.log(dp)
 }
-
 console.log(dp[input])
