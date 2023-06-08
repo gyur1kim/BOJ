2b4d1730d498fbc96b03d04271393cbc7490560b
gyur1kim <qri1114@gmail.com>
2023-06-08 23:52:50 +0900

    :recycle: 2667_코드수정
    - visited 배열을 없앰
    - 메모리, 시간 둘 다 감소

--- DFS BFS/2667_단지번호붙이기/2667_단지번호붙이기.js
+++ DFS BFS/2667_단지번호붙이기/2667_단지번호붙이기.js
@@ -9,7 +9,7 @@ let [n, ...maps] = `7
 
 n = Number(n);
 maps = maps.map(row => row.split('').map(Number));
-const visited = Array.from(Array(n), () => Array(n).fill(false));
+// const visited = Array.from(Array(n), () => Array(n).fill(false));   // visited를 없앰으로서 메모리 절약!
                     // 상,    하,     좌,       우
 const directions = [[0, -1], [0, 1], [-1, 0], [1, 0]];
 const answer = [];
@@ -17,8 +17,8 @@ const answer = [];
 
 for (let i=0; i<n; i++) {      // 행 : i
   for (let j=0; j<n; j++) {    // 열 : j
-    if (maps[i][j] === 1 && !visited[i][j]) {   // 집이 나왔는데, 방문하지 않았던 곳 => 새로운 단지의 시작
-      visited[i][j] = true;
+    if (maps[i][j] === 1) {    // 집 발견 === 새로운 단지
+      maps[i][j] = 0;          // 방문한 집은 0으로 변경
       const queue = [[i, j]];
       let head = 0
 
@@ -26,8 +26,8 @@ for (let i=0; i<n; i++) {      // 행 : i
         let [x, y] = queue[head++];
         for (let [dx, dy] of directions) {
           let [nx, ny] = [dx+x, dy+y];
-          if (0<=nx && nx<n && 0<=ny && ny<n && maps[nx][ny] === 1 && !visited[nx][ny]) {
-            visited[nx][ny] = true;
+          if (0<=nx && nx<n && 0<=ny && ny<n && maps[nx][ny] === 1) {
+            maps[nx][ny] = 0;
             queue.push([nx, ny]);
           }
         }
