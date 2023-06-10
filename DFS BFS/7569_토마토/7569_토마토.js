52f3331891f7b5b1920bc95877192a6b0fdd57d9
gyur1kim <qri1114@gmail.com>
2023-06-11 01:14:17 +0900

    :sparkles: 7569_토마토 - 메모리 초과

--- DFS BFS/7569_토마토/7569_토마토.js
+++ DFS BFS/7569_토마토/7569_토마토.js
@@ -1,22 +1,19 @@
-let [MNH, ...input] = `4 3 2
-1 1 1 1
-1 1 1 1
-1 1 1 1
-1 1 1 1
--1 -1 -1 -1
-1 1 1 -1`.split('\n');
+let [MNH, ...input] = `5 3 1
+0 -1 0 0 0
+-1 -1 0 1 1
+0 0 0 1 1`.split('\n');
 let [M, N, H] = MNH.split(' ').map(Number);
 input = input.map((tomatoRow) => tomatoRow.split(' ').map(Number))
-// console.log(input);
+
+
+/**  메모리 초과 뜸. 3중 배열 만드는게 오래 걸리는 것 같다.
+// 3중 배열 만들기
 const tomatoes = [];
 for (let i = 0; i < H; i ++) {
   tomatoes.push(input.slice(i*N, (i+1)*N));
 }
-// console.log(tomatoes);
+
 const directions = [[1, 0, 0], [-1, 0, 0], [0, 0, -1], [0, 0, 1], [0, 1, 0], [0, -1, 0]]   // 상 하 좌 우 앞 뒤
-
-
-/**  메모리 초과 뜸. 두 번 for문을 돌아서 메모리를 많이 차지한 것 아닐까?
 for (let h=0; h<H; h++) {             // 높이
   for (let i=0; i<N; i++) {           // 세로
     for (let j=0; j<M; j++) {         // 가로
@@ -64,5 +61,65 @@ for (let h=0; h<H; h++) {             // 높이
 }
 
 console.log(`${unripe? -1 : longestDay}`)
-
  */
+
+// 3중 배열 만들지 않고 구해보자!
+const directions = [[N, 0], [-N, 0], [0, -1], [0, 1], [1, 0], [-1, 0]]   // 상 하 좌 우 앞 뒤
+const arrLength = N*H
+for (let i=0; i<arrLength; i++) {     // 세로
+  for (let j=0; j<M; j++) {     // 가로
+    if (input[i][j] === 1) {    // 내가 탐색한 토마토가 1이면! 주변을 queue에 넣고 뒤져보기
+      const queue = [[i, j]];
+      let head = 0;
+
+      // bfs 시작뜅
+      while (queue.length > head) {
+        let [x, y] = queue[head++];
+
+        //1. 상, 하 탐색하기
+        for (let i=0; i<2; i++) {
+          let [nx, ny] = [x+directions[i][0], y+directions[i][1]];
+          if (0 <= nx && nx < arrLength && (input[nx][ny] === 0 || input[nx][ny] > input[x][y] + 1)) {
+            input[nx][ny] = input[x][y] + 1;
+            queue.push([nx, ny])
+          }
+        }
+        // 2. 좌, 우 탐색하기
+        for (let i=2; i<4; i++) {
+          let [nx, ny] = [x+directions[i][0], y+directions[i][1]];
+          if (0 <= ny && ny < M && (input[nx][ny] === 0 || input[nx][ny] > input[x][y] + 1)) {
+            input[nx][ny] = input[x][y] + 1;
+            queue.push([nx, ny])
+          }
+        }
+        // 3. 앞, 뒤 탐색
+        for (let i=4; i<6; i++) {
+          let [nx, ny] = [x+directions[i][0], y+directions[i][1]];
+          let h = Math.floor(x/N);
+          if (h*N <= nx && nx < (h+1)*N && (input[nx][ny] === 0 || input[nx][ny] > input[x][y] + 1)) {
+            input[nx][ny] = input[x][y] + 1;
+            queue.push([nx, ny])
+          }
+        }
+      }
+
+    }
+  }
+}
+
+let unripe = false;
+let longestDay = 0;
+
+for (let i=0; i<arrLength; i++) {     // 세로
+  for (let j=0; j<M; j++) {           // 가로
+    if (input[i][j] === 0) {
+      unripe = true;
+      break;
+    }
+    else if (input[i][j] - 1 > longestDay) {
+      longestDay = input[i][j] - 1
+    }
+  }
+  if (unripe) break;
+}
+console.log(`${unripe? -1 : longestDay}`)
