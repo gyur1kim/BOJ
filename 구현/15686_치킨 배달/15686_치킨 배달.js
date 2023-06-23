1bb106efb40f0d4dbd9baaf43bbc985125062c37
gyur1kim <qri1114@gmail.com>
2023-06-23 10:28:50 +0900

    :recycle: 15686_치킨 배달 시간 단축
    - 208ms로 단축
    - M개의 치킨집을 선택한 후, 각 집에서 치킨집까지의 최단 거리를 구해 합계를 낸다.
    - 기존 치킨거리와 새롭게 구한 치킨거리를 비교하여, 최종적으로 가장 짧은 치킨 거리를 구한다.

--- 구현/15686_치킨 배달/15686_치킨 배달.js
+++ 구현/15686_치킨 배달/15686_치킨 배달.js
@@ -30,79 +30,93 @@ let [N, M] = NM.split(' ').map(Number);
 input = input.map(r => r.split(' ').map(Number));
 
 let chickenDist = Number.MAX_SAFE_INTEGER;
-const chickenCoord = [];        // 치킨집
-const chooseChickenCoord = [];  // 선택된 치킨집
-const houseCoord = [];          // 집의 위치
+const chickenCoord = [];        // 전체 치킨집 좌표
+const selectChickenCoord = [];  // 선택된 치킨집 좌표
+const houseCoord = [];          // 집 좌표
+// let countHouse = 0;             // 집 개수
 
 // 치킨집의 위치와 집의 위치 기억하기.
 for (let i=0; i<N; i++) {
   for (let j=0; j<N; j++) {
+    // if (input[i][j] === 1) countHouse++;
     if (input[i][j] === 1) houseCoord.push([i, j]);
     else if (input[i][j] === 2) chickenCoord.push([i, j]);
   }
 }
 
-// let visited = new Map();
-// for (let house of houseCoord) {
-//   visited.set(house, false);
-// }
-//
-// function goFurther (x, y, originX, originY, dist) {
-//   if (x < 0 || x >= N || y < 0 || y >= N) return;
-//
-//   if (visited.has([x, y])) {
-//     visited.delete([x, y]);
-//     tmpChickenDist += (Math.abs(originX - x) + Math.abs(originY - y));
+
+// const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+// function bfs() {
+//   let tmpChickenDist = 0;   // 임시 치킨거리
+//   let countHouseCopy = countHouse;  // 임시 집 개수
+//   const visited = Array.from(new Array(N), () => Array(N).fill(false));   // 방문 2차배열 생성
+//   for (let [x, y] of selectChickenCoord) {
+//     visited[x][y] = 0;
 //   }
 //
+//   const queue = [...selectChickenCoord];
+//   let head = 0;
+//
+//   while (queue.length > head) {
+//     let [x, y] = queue[head++];
+//
+//     for (let [dx, dy] of directions) {
+//       let [nx, ny] = [x+dx, y+dy];    // 치킨집에서 하나씩 bfs하기
+//       if (0<=nx && nx<N && 0<=ny && ny<N && visited[nx][ny] === false) {
+//         if (input[nx][ny] === 1) {   // 1이면 (가정집을 찾았으면!)
+//           tmpChickenDist += visited[x][y] + 1;
+//           if (--countHouseCopy === 0) {  // 첫 번째 조건, 모든 집을 다 찾았으면 return하기 => 약 20ms 감소
+//             if (tmpChickenDist < chickenDist) chickenDist = tmpChickenDist;
+//             return;
+//           }
+//           else if (tmpChickenDist >= chickenDist) return;   // 두 번째 조건, 임시 치킨 거리가 기존의 치킨거리보다 길어지면 멈추기.  => 오히려 시간 증가.
+//         }
+//         visited[nx][ny] = visited[x][y] + 1;
+//         queue.push([nx, ny]);
+//       }
+//     }
+//   }
+//
+//   if (tmpChickenDist < chickenDist) chickenDist = tmpChickenDist;
+//
 // }
+function calcDist(x1, y1, x2, y2) {
+  return Math.abs(x1-x2) + Math.abs(y1-y2);
+}
 
-const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
 
-function bfs() {
-  let tmpChickenDist = 0;   // 임시 치킨거리
-  const visited = Array.from(new Array(N), () => Array(N).fill(false));   // 방문 2차배열 생성
-  for (let [x, y] of chooseChickenCoord) {
-    visited[x][y] = 0;
-  }
+function getChickenDist () {
+  let dist, tmpChickenDist = 0;
 
-  const queue = [...chooseChickenCoord];
-  let head = 0;
-
-  while (queue.length > head) {
-    let [x, y] = queue[head++];
-
-    for (let [dx, dy] of directions) {
-      let [nx, ny] = [x+dx, y+dy];    // 치킨집에서 하나씩 bfs하기
-      if (0<=nx && nx<N && 0<=ny && ny<N && visited[nx][ny] === false) {
-        if (input[nx][ny] === 1) {   // 1이면 (가정집을 찾았으면!)
-          tmpChickenDist += visited[x][y] + 1;
-        }
-        visited[nx][ny] = visited[x][y] + 1;
-        queue.push([nx, ny]);
+  for (let [houseX, houseY] of houseCoord) {                 // 한 집에 대해서
+    let minDist = Number.MAX_SAFE_INTEGER;
+    for (let [chickenX, chickenY] of selectChickenCoord) {   // 모든 치킨집과의 거리를 구하고, 가장 짧은 거리를 저장하자.
+      dist = calcDist(houseX, houseY, chickenX, chickenY);
+      if (dist < minDist) {
+        minDist = dist
       }
     }
+    tmpChickenDist += minDist;   // 가장 짧았던 거리를 임시 치킨거리에 누적.
   }
 
   if (tmpChickenDist < chickenDist) chickenDist = tmpChickenDist;
-
 }
-
 
 // 살아남을 M개의 치킨집 고르기
 function chooseChicken (count, idx) {
   if (count === M) {
-    // 치킨집 다 찾았으니까 bfs 시작하기.
-    console.log(chooseChickenCoord);
-    bfs();
+    // 치킨집 다 정했으니까 치킨거리 구하기;
+    // bfs();
+    getChickenDist();
     return;
   }
 
   for (let i=idx; i<chickenCoord.length; i++) {
-    // if (!chooseChickenCoord.includes(chickenCoord[i])) {
-      chooseChickenCoord.push(chickenCoord[i]);
+    // if (!selectChickenCoord.includes(chickenCoord[i])) {
+      selectChickenCoord.push(chickenCoord[i]);
       chooseChicken(count+1, i+1);  // 살아남을 치킨집에 추가
-      chooseChickenCoord.pop();      // 빼기
+      selectChickenCoord.pop();      // 빼기
     // }
   }
 }
