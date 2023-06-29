790751c59aca4bd956c7b2067c9bec1686930a53
gyur1kim <qri1114@gmail.com>
2023-06-29 18:58:45 +0900

    :recycle: 14503_청소기 시간 단축
    - dfs방식은, 함수 호출을 스택에 저장하기 때문에 메모리와 시간 둘 다 많이 든다.
    - while방식을 이용해 메모리와 시간 둘 다 감소
    - 메모리 : 9804KB -> 9384KB
    - 시간 : 152ms -> 128ms

--- 구현/14503_로봇 청소기/14503_로봇 청소기.js
+++ 구현/14503_로봇 청소기/14503_로봇 청소기.js
@@ -65,7 +65,7 @@
 // let [N, M] = NM.split(' ').map(Number);
 // let [r, c, d] = rcd.split(' ').map(Number);
 // input = input.map(row => row.split(' ').map(Number));
-//
+
 // // 0은 청소하지 않은 곳, 1은 벽, 2는 방문한 곳
 // let directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];  // 북, 동, 남, 서(시계방향임)
 // function dfs(x, y, d) {
@@ -96,52 +96,110 @@
 // dfs(r, c, d);      // 여기서부터 청소를 시작할거야
 // console.log(cleanCnt)
 
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+//
+// let input = [];
+// rl.on('line', function (line) {
+//   input.push(line);
+// })
+//   .on('close', function () {
+//     let cleanCnt = 0;
+//     let [N, M] = input.shift().split(' ').map(Number);
+//     let [r, c, d] = input.shift().split(' ').map(Number);
+//     input = input.map(row => row.split(' ').map(Number));
+//
+//     // 0은 청소하지 않은 곳, 1은 벽, 2는 방문한 곳
+//     let directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];  // 북, 동, 남, 서(시계방향임)
+//     function dfs(x, y, d) {
+//       console.log(`{${x}, ${y}}, ${cleanCnt}`)
+//       for (let i=d-1; i>d-5; i--) {  // 반시계방향으로 90도 돌리고 시작하자. 0=> 3, 2, 1, 0 / 2=> 1, 0, 3, 2 ...
+//         let idx = (4+i)%4;
+//         let [nx, ny] = [x+directions[idx][0], y+directions[idx][1]];
+//         console.log(`다음 방향의 값은? ${input[nx][ny]}`)
+//         if (input[nx][ny] === 0) { // 안 간 곳이 있으면
+//           input[nx][ny] = 2;       // 방문 처리
+//           cleanCnt++;              // 청소했어요!
+//           dfs(nx, ny, idx)         // 다시 여기부터 봐보자!
+//         }
+//       }
+//
+//       // 주변을 다 청소했으면 뒤로 갑시다.
+//       let idx = d >= 2? d-2 : d+2;
+//       let [nx, ny] = [x+directions[idx][0], y+directions[idx][1]];
+//       if (input[nx][ny] === 1) {
+//         console.log(cleanCnt);
+//         process.exit();
+//       }
+//       else dfs(nx, ny, d)
+//     }
+//
+//     input[r][c] = 2;
+//     cleanCnt++;        // 청소기 놓인 곳부터 청소합시다
+//     dfs(r, c, d);      // 여기서부터 청소를 시작할거야
+//
+//
+//     console.log(cleanCnt)
+//     process.exit();
+//   });
 
-let input = [];
-rl.on('line', function (line) {
-  input.push(line);
-})
-  .on('close', function () {
-    let cleanCnt = 0;
-    let [N, M] = input.shift().split(' ').map(Number);
-    let [r, c, d] = input.shift().split(' ').map(Number);
-    input = input.map(row => row.split(' ').map(Number));
+let [NM, rcd, ...input] = `11 10
+7 4 0
+1 1 1 1 1 1 1 1 1 1
+1 0 0 0 0 0 0 0 0 1
+1 0 0 0 1 1 1 1 0 1
+1 0 0 1 1 0 0 0 0 1
+1 0 1 1 0 0 0 0 0 1
+1 0 0 0 0 0 0 0 0 1
+1 0 0 0 0 0 0 1 0 1
+1 0 0 0 0 0 1 1 0 1
+1 0 0 0 0 0 1 1 0 1
+1 0 0 0 0 0 0 0 0 1
+1 1 1 1 1 1 1 1 1 1`.split('\n');
+let [N, M] = NM.split(' ').map(Number);
+let [r, c, d] = rcd.split(' ').map(Number);
+input = input.map(row => row.split(' ').map(Number));
 
-    // 0은 청소하지 않은 곳, 1은 벽, 2는 방문한 곳
-    let directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];  // 북, 동, 남, 서(시계방향임)
-    function dfs(x, y, d) {
-      console.log(`{${x}, ${y}}, ${cleanCnt}`)
-      for (let i=d-1; i>d-5; i--) {  // 반시계방향으로 90도 돌리고 시작하자. 0=> 3, 2, 1, 0 / 2=> 1, 0, 3, 2 ...
-        let idx = (4+i)%4;
-        let [nx, ny] = [x+directions[idx][0], y+directions[idx][1]];
-        console.log(`다음 방향의 값은? ${input[nx][ny]}`)
-        if (input[nx][ny] === 0) { // 안 간 곳이 있으면
-          input[nx][ny] = 2;       // 방문 처리
-          cleanCnt++;              // 청소했어요!
-          dfs(nx, ny, idx)         // 다시 여기부터 봐보자!
-        }
-      }
+input[r][c] = 2;   // 청소기가 있는 곳은 청소했으므로 2
+let cleanCnt = 1;  // 청소한 곳의 개수
+let directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];  // 북, 동, 남, 서(시계방향임)
 
-      // 주변을 다 청소했으면 뒤로 갑시다.
-      let idx = d >= 2? d-2 : d+2;
-      let [nx, ny] = [x+directions[idx][0], y+directions[idx][1]];
-      if (input[nx][ny] === 1) {
-        console.log(cleanCnt);
-        process.exit();
-      }
-      else dfs(nx, ny, d)
+function turn() {
+  // 3->2, 2->1, 1->0, 0->3, 3->2... 나머지 연산을 이용해보자.
+  d = (d+3)%4
+}
+
+// 단순 x, y의 값을 변화하는 방식
+// while문을 이용해 계속 돌면 된다.
+let nr, nc;         // 다음 청소기 위치
+let turnCnt = 0;    // 4번 돌면 뒤로 갈거임.
+while (true) {
+  turn();                    // 90도 회전
+  turnCnt++;
+  nr = r + directions[d][0];
+  nc = c + directions[d][1];
+
+  if (input[nr][nc] === 0) {
+    input[nr][nc] = 2   // 청소하기
+    cleanCnt++;         // 청소한 구역 +1
+    r = nr
+    c = nc
+    turnCnt = 0;        // 횟수 초기화
+  }
+
+  if (turnCnt === 4) {  // 4군데 다 돌아봤으면? 뒤로 가자.
+    nr = r - directions[d][0];
+    nc = c - directions[d][1];
+    if (input[nr][nc] === 1) break;   // 뒤로 갈 곳이 벽이면 종료
+    else {
+      r = nr
+      c = nc
+      turnCnt = 0;        // 횟수 초기화
     }
+  }
+}
 
-    input[r][c] = 2;
-    cleanCnt++;        // 청소기 놓인 곳부터 청소합시다
-    dfs(r, c, d);      // 여기서부터 청소를 시작할거야
-
-
-    console.log(cleanCnt)
-    process.exit();
-  });
+console.log(cleanCnt);
