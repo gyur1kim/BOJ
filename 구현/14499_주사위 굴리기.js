c893052f6c4be77517c4f9d97865d76b7861ac82
gyur1kim <qri1114@gmail.com>
2025-01-05 10:45:51 +0900

    refactor: 주사위 굴리기 리팩토링
    - 위 아래 동서남북을 index로 관리하지 말고, 그냥 dice 내부에서 섞음
    - console.log도 시간이 많이 걸리므로, result 문자열에 저장했다가 한 번에 출력

--- 구현/14499_주사위 굴리기.js
+++ 구현/14499_주사위 굴리기.js
@@ -1,48 +1,13 @@
 function solution(N, M, x, y, map, directions) {
-  const dice = [0, 0, 0, 0, 0, 0, 0];
-
-  const indices = {
-    top: 1,
-    east: 3,
-    west: 4,
-    south: 5,
-    north: 2,
-    bottom: 6,
-  };
+  // 위, 북, 동, 서, 남, 바닥
+  let dice = [0, 0, 0, 0, 0, 0];
+  let result = "";
 
   const nextMove = {
     1: [0, 1],
     2: [0, -1],
     3: [-1, 0],
     4: [1, 0],
-  };
-
-  // updates는 value값이 key값으로 굴러가는거임
-  const rotatePatterns = {
-    1: {
-      east: "top",
-      top: "west",
-      west: "bottom",
-      bottom: "east",
-    },
-    2: {
-      west: "top",
-      top: "east",
-      east: "bottom",
-      bottom: "west",
-    },
-    3: {
-      north: "top",
-      top: "south",
-      south: "bottom",
-      bottom: "north",
-    },
-    4: {
-      south: "top", // top이 south가 된다는 뜻
-      top: "north",
-      north: "bottom",
-      bottom: "south",
-    },
   };
 
   for (const direction of directions) {
@@ -52,22 +17,30 @@ function solution(N, M, x, y, map, directions) {
     // 범위를 벗어나면 출력도 하지 말고 무시
     if (!inRange(N, M, nx, ny)) continue;
 
-    const pattern = rotatePatterns[direction];
-
-    const nextBottomIndex = indices[pattern.bottom];
-    copyBottom(map, nx, ny, dice, nextBottomIndex);
-
-    // 주사위 굴리면서 index 업데이트
-    for (const [to, from] of Object.entries(pattern)) {
-      if (to === "bottom") indices.bottom = nextBottomIndex;
-      else indices[to] = indices[from];
+    const [x1, x2, x3, x4, x5, x6] = dice;
+    switch (direction) {
+      case 1:
+        dice = [x4, x2, x1, x6, x5, x3];
+        break;
+      case 2:
+        dice = [x3, x2, x6, x1, x5, x4];
+        break;
+      case 3:
+        dice = [x5, x1, x3, x4, x6, x2];
+        break;
+      case 4:
+        dice = [x2, x6, x3, x4, x1, x5];
+        break;
     }
 
-    console.log(dice[indices.top]);
+    copyBottom(map, nx, ny, dice, 5);
 
+    result += `${dice[0]}\n`;
     x = nx;
     y = ny;
   }
+
+  return result;
 }
 
 function inRange(N, M, nx, ny) {
@@ -96,4 +69,5 @@ const [N, M, x, y, K] = input[0].split(" ").map(Number);
 const map = input.slice(1, 1 + N).map((l) => l.split(" ").map(Number));
 const directions = input[1 + N].split(" ").map(Number);
 
-solution(N, M, x, y, map, directions);
+const result = solution(N, M, x, y, map, directions);
+console.log(result);
