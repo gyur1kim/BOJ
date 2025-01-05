da04c4fdd631a2fba1c939e595d557c36d9494aa
gyur1kim <qri1114@gmail.com>
2025-01-05 10:25:33 +0900

    refactor: 반복 부분 추상화

--- 구현/14499_주사위 굴리기.js
+++ 구현/14499_주사위 굴리기.js
@@ -1,19 +1,48 @@
 function solution(N, M, x, y, map, directions) {
-  // 주사위 전개도 숫자들 있져? 그거 idx맞춰서 봐
-  // 그래서 idx 0은 걍 빈 값임
-  // 아래, 북쪽, 동쪽, 서쪽, 남쪽, 위
   const dice = [0, 0, 0, 0, 0, 0, 0];
-  let topIndex = 1;
-  let eastIndex = 3;
-  let westIndex = 4;
-  let southIndex = 5;
-  let northIndex = 2;
+
+  const indices = {
+    top: 1,
+    east: 3,
+    west: 4,
+    south: 5,
+    north: 2,
+    bottom: 6,
+  };
 
   const nextMove = {
-    1: [0, 1], // 동
-    2: [0, -1], // 서
-    3: [-1, 0], // 북
-    4: [1, 0], // 남
+    1: [0, 1],
+    2: [0, -1],
+    3: [-1, 0],
+    4: [1, 0],
+  };
+
+  // updates는 value값이 key값으로 굴러가는거임
+  const rotatePatterns = {
+    1: {
+      east: "top",
+      top: "west",
+      west: "bottom",
+      bottom: "east",
+    },
+    2: {
+      west: "top",
+      top: "east",
+      east: "bottom",
+      bottom: "west",
+    },
+    3: {
+      north: "top",
+      top: "south",
+      south: "bottom",
+      bottom: "north",
+    },
+    4: {
+      south: "top", // top이 south가 된다는 뜻
+      top: "north",
+      north: "bottom",
+      bottom: "south",
+    },
   };
 
   for (const direction of directions) {
@@ -23,54 +52,18 @@ function solution(N, M, x, y, map, directions) {
     // 범위를 벗어나면 출력도 하지 말고 무시
     if (!inRange(N, M, nx, ny)) continue;
 
-    let tmpTopIndex = topIndex;
-    if (direction === 1) {
-      // 동쪽으로 굴리면
-      // 원래 동쪽에 있던 애가 바닥으로 가게 됨 -> dice 업데이트
-      copyBottom(map, nx, ny, dice, eastIndex);
+    const pattern = rotatePatterns[direction];
 
-      // 기존의 top -> 동쪽
-      // 기존의 서쪽 -> top
-      // 기존의 bottom(== 7 - 기존top) -> 서쪽
-      eastIndex = topIndex;
-      topIndex = westIndex;
-      westIndex = 7 - tmpTopIndex;
-    } else if (direction === 2) {
-      // 서쪽으로 굴리면
-      // 원래 서쪽에 있던 애가 바닥으로 가게 됨 -> dice 업데이트
-      copyBottom(map, nx, ny, dice, westIndex);
+    const nextBottomIndex = indices[pattern.bottom];
+    copyBottom(map, nx, ny, dice, nextBottomIndex);
 
-      // 기존의 top -> 서쪽
-      // 기존의 동쪽 -> top
-      // 기존의 bottom(== 7 - top) -> 동쪽
-      westIndex = topIndex;
-      topIndex = eastIndex;
-      eastIndex = 7 - tmpTopIndex;
-    } else if (direction === 3) {
-      // 뷱쪽으로 굴리면
-      // 원래 북쪽에 있던 애가 바닥으로 가게 됨 -> dice 업데이트
-      copyBottom(map, nx, ny, dice, northIndex);
-
-      // 기존의 top -> 북쪽
-      // 기존의 남쪽 -> top
-      // 기존의 bottom(== 7 - top) -> 남쪽
-      northIndex = topIndex;
-      topIndex = southIndex;
-      southIndex = 7 - tmpTopIndex;
-    } else if (direction === 4) {
-      // 남쪽으로 굴리면
-      // 원래 남쪽에 있던 애가 바닥으로 가게 됨 -> dice 업데이트
-      copyBottom(map, nx, ny, dice, southIndex);
-
-      // 기존의 top -> 남쪽
-      // 기존의 북쪽 -> top
-      // 기존의 bottom(== 7 - top) -> 북쪽
-      southIndex = topIndex;
-      topIndex = northIndex;
-      northIndex = 7 - tmpTopIndex;
+    // 주사위 굴리면서 index 업데이트
+    for (const [to, from] of Object.entries(pattern)) {
+      if (to === "bottom") indices.bottom = nextBottomIndex;
+      else indices[to] = indices[from];
     }
 
-    console.log(dice[topIndex]);
+    console.log(dice[indices.top]);
 
     x = nx;
     y = ny;
