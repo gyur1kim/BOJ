f627567b3408602e35849547fed85ec96bb476be
gyur1kim <qri1114@gmail.com>
2025-01-12 09:38:00 +0900

    refactor: 톱니바퀴 리팩토링
    - top index 하나만 가지고 right, left까지 관리하도록 수정
    - gears배열에서 맨 앞의 0 제거

--- 구현/14891_톱니바퀴.js
+++ 구현/14891_톱니바퀴.js
@@ -1,8 +1,7 @@
 class Gear {
-  constructor(gear, left, right) {
+  constructor(gear) {
     this.gear = gear;
-    this.left = left;
-    this.right = right;
+    this.top = 0;
   }
 
   turn(direction) {
@@ -11,25 +10,25 @@ class Gear {
   }
 
   clockwise() {
-    this.left = (this.left - 1 + 8) % 8;
-    this.right = (this.right - 1 + 8) % 8;
+    this.top = (this.top - 1 + 8) % 8;
   }
 
   counterclockwise() {
-    this.left = (this.left + 1) % 8;
-    this.right = (this.right + 1) % 8;
+    this.top = (this.top + 1) % 8;
   }
 
   getLeft() {
-    return this.gear[this.left];
+    const left = (this.top + 6) % 8;
+    return this.gear[left];
   }
 
   getRight() {
-    return this.gear[this.right];
+    const right = (this.top + 2) % 8;
+    return this.gear[right];
   }
 
   getTop() {
-    return Number(this.gear[(this.right - 2 + 8) % 8]);
+    return Number(this.gear[this.top]);
   }
 }
 
@@ -40,7 +39,7 @@ function turnGear(gearNumber, direction) {
   let tmpDirection = direction;
 
   // 왼쪽을 보자
-  for (let i = gearNumber - 1; i > 0; i--) {
+  for (let i = gearNumber - 1; i >= 0; i--) {
     const leftGear = gears[i];
     const gear = gears[i + 1];
 
@@ -53,7 +52,7 @@ function turnGear(gearNumber, direction) {
 
   // 기어 오른쪽을 보자
   tmpDirection = direction;
-  for (let i = gearNumber + 1; i <= 4; i++) {
+  for (let i = gearNumber + 1; i < 4; i++) {
     const rightGear = gears[i];
     const gear = gears[i - 1];
 
@@ -83,21 +82,16 @@ const input = `10101110
 const tries = input.slice(4);
 const tryCount = Number(tries.shift());
 
-const gears = [0];
+const gears = [];
 for (let i = 0; i < 4; i++) {
-  gears.push(new Gear(input[i], 6, 2));
+  gears.push(new Gear(input[i]));
 }
 
 for (let tc = 0; tc < tryCount; tc++) {
   const [gearNumber, direction] = tries[tc].split(" ").map(Number);
-
-  turnGear(gearNumber, direction);
+  turnGear(gearNumber - 1, direction);
 }
 
-gears.shift();
-const result = gears.reduce(
-  (sum, gear, idx) => (sum += gear.getTop() ? 2 ** idx : 0),
-  0
-);
+const result = gears.reduce((sum, gear, idx) => (sum += gear.getTop() ? 2 ** idx : 0), 0);
 
 console.log(result);
