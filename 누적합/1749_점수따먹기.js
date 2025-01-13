3e5db3ab9613e2e244a16094b27cbebc29efd929
gyur1kim <qri1114@gmail.com>
2025-01-13 11:11:07 +0900

    refactor: solution함수 제거
    - 제거했더니 약 0.1ms 빨라짐
    - 유의미하지는 않은듯

--- 누적합/1749_점수따먹기.js
+++ 누적합/1749_점수따먹기.js
@@ -1,43 +1,29 @@
-function solution(N, M, matrix) {
-  const prefixMatrix = Array(N + 1)
-    .fill()
-    .map(() => Array(M + 1).fill(0));
-  let answer = -Infinity;
+const [[N, M], ...input] = require("fs")
+  .readFileSync(0, "utf-8")
+  .trim()
+  .split("\n")
+  .map(e => e.split(" ").map(Number));
+const matrix = Array(N + 1)
+  .fill()
+  .map(() => Array(M + 1).fill(0));
+let answer = -Infinity;
 
-  for (let i = 1; i < N + 1; i++) {
-    for (let j = 1; j < M + 1; j++) {
-      prefixMatrix[i][j] =
-        prefixMatrix[i - 1][j] +
-        prefixMatrix[i][j - 1] -
-        prefixMatrix[i - 1][j - 1] +
-        matrix[i - 1][j - 1];
-    }
+for (let i = 1; i < N + 1; i++) {
+  for (let j = 1; j < M + 1; j++) {
+    matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1] + input[i - 1][j - 1];
   }
+}
 
-  for (let a = 1; a < N + 1; a++) {
-    for (let b = 1; b < M + 1; b++) {
-      for (let c = a; c < N + 1; c++) {
-        for (let d = b; d < M + 1; d++) {
-          const prefixSum =
-            prefixMatrix[c][d] -
-            prefixMatrix[a - 1][d] -
-            prefixMatrix[c][b - 1] +
-            prefixMatrix[a - 1][b - 1];
+for (let a = 1; a < N + 1; a++) {
+  for (let b = 1; b < M + 1; b++) {
+    for (let c = a; c < N + 1; c++) {
+      for (let d = b; d < M + 1; d++) {
+        const prefixSum = matrix[c][d] - matrix[a - 1][d] - matrix[c][b - 1] + matrix[a - 1][b - 1];
 
-          answer = Math.max(prefixSum, answer);
-        }
+        answer = Math.max(prefixSum, answer);
       }
     }
   }
-
-  console.log(answer);
 }
 
-const [NM, ...inputs] = `2 4
-3 -5 0 5
-3 2 2 -5`.split("\n");
-
-const [N, M] = NM.split(" ").map(Number);
-const matrix = inputs.map(input => input.split(" ").map(Number));
-
-solution(N, M, matrix);
+console.log(answer);
