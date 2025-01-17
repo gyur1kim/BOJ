a7904efcacc5cca210dd42c27eb5b4d2311f8644
gyur1kim <qri1114@gmail.com>
2025-01-18 00:59:49 +0900

    refactor: 공유기 설치 리팩토링
    - 공유기 간격을 지정하는 start값과 end값 수정
    - 공유기 설치해보는 부분 함수화

--- 이분탐색/2110_공유기 설치.js
+++ 이분탐색/2110_공유기 설치.js
@@ -1,40 +1,40 @@
 function solution(N, C, houses) {
   houses.sort((a, b) => a - b);
 
-  let start = 0;
-  let end = houses[N - 1];
+  let start = 1;
+  let end = houses[N - 1] - houses[0];
   let maxGap = -Infinity;
 
   while (start <= end) {
     let gap = Math.ceil((end + start) / 2);
-    let tmpStartIdx = 0;
-    let tmpNextIdx = 1;
-    let tmpC = C - 1;
 
-    while (tmpNextIdx < N) {
-      console.log("tmpStartIdx, tmpNextIdx", tmpStartIdx, tmpNextIdx);
-      console.log("tmpC", tmpC);
-      if (houses[tmpNextIdx] - houses[tmpStartIdx] < gap) {
-        tmpNextIdx++;
-        continue;
-      }
-
-      tmpC--;
-      tmpStartIdx = tmpNextIdx;
-      tmpNextIdx = tmpStartIdx + 1;
+    // 공유기 안남아요 => 잘 설치했어요
+    if (setC(N, C, houses, gap)) {
+      maxGap = Math.max(maxGap, gap);
+      start = gap + 1;
     }
-
-    // 공유기가 남아요 > gap을 줄입시다.
-    if (tmpC > 0) {
+    // 공유기가 남아요 => gap을 줄입시다.
+    else {
       end = gap - 1;
-      continue;
     }
-
-    maxGap = Math.max(maxGap, gap);
-    start = gap + 1;
   }
 
   console.log(maxGap);
+}
+
+function setC(N, C, houses, gap) {
+  let cntC = 1;
+  let prevHouse = houses[0];
+
+  for (let i = 1; i < N; i++) {
+    if (houses[i] - prevHouse >= gap) {
+      cntC++;
+      prevHouse = houses[i];
+    }
+  }
+
+  // 설치된 공유기 개수가 C개 이상인지? (== 공유기가 남진 않는거지?)
+  return cntC >= C;
 }
 
 const [N, C, ...houses] = `5 3
