6d52e941a5d9c3cb8bb42c850c2b705a776e4b4b
gyur1kim <qri1114@gmail.com>
2025-02-03 15:52:11 +0900

    feat: 욕심쟁이 판다 통과
    - 첫 시도는 매 단계에서 4방향을 개별 DFS로 호출 (기하급수적 증가)
    - 두 번째 시도는 한 번의 DFS 안에서 4방향을 순차적으로 처리 (선형적 증가)

--- DP/1937_욕심쟁이 판다.js
+++ DP/1937_욕심쟁이 판다.js
@@ -1,22 +1,3 @@
-function dfs(i, j, prevBamboo) {
-  if (i < 0 || i >= N || j < 0 || j >= N) return -1;
-
-  const bamboo = map[i][j];
-  if (bamboo < prevBamboo) return -1;
-
-  if (visited[i][j] === -1) {
-    visited[i][j] = Math.max(
-      dfs(i, j - 1, bamboo),
-      dfs(i, j + 1, bamboo),
-      dfs(i - 1, j, bamboo),
-      dfs(i + 1, j, bamboo),
-      1
-    );
-  }
-
-  return visited[i][j] + 1;
-}
-
 const [n, ...input] = `4
 14 9 12 10
 1 11 5 4
@@ -24,28 +5,93 @@ const [n, ...input] = `4
 6 3 16 8`.split("\n");
 const N = Number(n);
 const map = input.map(i => i.split(" ").map(Number));
+
 const visited = Array(N)
   .fill()
   .map(() => Array(N).fill(-1));
+const directions = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+let answer = 1;
 
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    // 여기 방문했었으면 건너뜀
-    if (visited[i][j] !== -1) continue;
+function dfs(i, j) {
+  if (visited[i][j] === -1) {
+    visited[i][j] = 0;
+    for (const [dx, dy] of directions) {
+      const [nx, ny] = [i + dx, j + dy];
 
-    const bamboo = map[i][j];
-    visited[i][j] = Math.max(
-      dfs(i, j - 1, bamboo),
-      dfs(i, j + 1, bamboo),
-      dfs(i - 1, j, bamboo),
-      dfs(i + 1, j, bamboo),
-      1
-    );
+      if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+      if (map[nx][ny] > map[i][j]) {
+        visited[i][j] = Math.max(visited[i][j], dfs(nx, ny));
+      }
+    }
+  }
+
+  return visited[i][j] + 1;
+}
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    answer = Math.max(answer, dfs(i, j));
   }
 }
 
-const answer = visited
-  .flat()
-  .sort((a, b) => a - b)
-  .pop();
 console.log(answer);
+
+// function dfs(i, j, prevBamboo) {
+//   if (i < 0 || i >= N || j < 0 || j >= N) return -1;
+
+//   const bamboo = map[i][j];
+//   if (bamboo < prevBamboo) return -1;
+
+//   console.log(prevBamboo, bamboo);
+
+//   if (visited[i][j] === -1) {
+//     visited[i][j] = Math.max(
+//       dfs(i, j - 1, bamboo),
+//       dfs(i, j + 1, bamboo),
+//       dfs(i - 1, j, bamboo),
+//       dfs(i + 1, j, bamboo),
+//       1
+//     );
+//   }
+
+//   return visited[i][j] + 1;
+// }
+
+// const [n, ...input] = `4
+// 14 9 12 10
+// 1 11 5 4
+// 7 15 2 13
+// 6 3 16 8`.split("\n");
+// const N = Number(n);
+// const map = input.map(i => i.split(" ").map(Number));
+// const visited = Array(N)
+//   .fill()
+//   .map(() => Array(N).fill(-1));
+
+// for (let i = 0; i < N; i++) {
+//   for (let j = 0; j < N; j++) {
+//     // 여기 방문했었으면 건너뜀
+//     if (visited[i][j] !== -1) continue;
+
+//     const bamboo = map[i][j];
+//     visited[i][j]function dfs(x, y) {
+//   if (dp[x][y] === -1) {
+//     dp[x][y] = 0;
+//     for (let k = 0; k < 4; k++) {
+//       let nx = x + dx[k];
+//       let ny = y + dy[k];
+//       if (0 <= nx && 0 <= ny && nx < n && ny < n) {
+//         if (board[nx][ny] > board[x][y]) {
+//           dp[x][y] = Math.max(dp[x][y], dfs(nx, ny));
+//         }
+//       }
+//     }
+//   }
+//   return dp[x][y] + 1;
+// }
