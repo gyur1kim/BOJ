d441176532b1cce121c50bbc77cb5a841b9958dd
gyur1kim <qri1114@gmail.com>
2025-03-08 23:56:49 +0900

    fix: 넴모넴모(easy)
    - N * M이 25이하로 제한되어 재귀함수가 터지지 않음.
    - 특정 r, c에 넴모를 넣어보고 다음 칸으로 dfs 진행
    - 혹은 특정 r, c에 넴모를 넣지 않고 다음 칸으로 dfs 진행
    - 모든 칸을 채운 뒤, 2*2가 되는 칸이 있는지 확인

--- 14712_넴모넴모.js
+++ 14712_넴모넴모.js
@@ -1,74 +1,37 @@
-const [N, M] = `25 25`.split(" ").map(Number);
-const TOTAL = M * N;
-const NEMO_BOARD = Array(N)
-  .fill()
-  .map(() => Array(M).fill(0));
-let answer = 1 + TOTAL; // 아무것도 안 놓은 경우 게임 종료 + 1개만 놓은 경우 게임 종료
+function dfs(r, c) {
+  if (r === N) {
+    for (let i = 0; i < N - 1; i++) {
+      for (let j = 0; j < M - 1; j++) {
+        if (
+          NEMO_BOARD[i][j] &&
+          NEMO_BOARD[i + 1][j] &&
+          NEMO_BOARD[i][j + 1] &&
+          NEMO_BOARD[i + 1][j + 1]
+        ) {
+          return;
+        }
+      }
+    }
 
-function checkAround(i, j) {
-  return (
-    checkLeftTop(i, j) && checkLeftBottom(i, j) && checkRightTop(i, j) && checkRightBottom(i, j)
-  );
-}
-
-function checkInRange(nr, nc) {
-  return 0 <= nr && nr < N && 0 <= nc && nc < M;
-}
-
-function checkLeftTop(i, j) {
-  let cntNemo = 0;
-  if (checkInRange(i - 1, j - 1) && checkInRange(i - 1, j) && checkInRange(i, j - 1)) {
-    cntNemo = NEMO_BOARD[i - 1][j - 1] + NEMO_BOARD[i - 1][j] + NEMO_BOARD[i][j - 1];
-  }
-  return cntNemo !== 3;
-}
-
-function checkRightTop(i, j) {
-  let cntNemo = 0;
-  if (checkInRange(i - 1, j) && checkInRange(i - 1, j + 1) && checkInRange(i, j + 1)) {
-    cntNemo = NEMO_BOARD[i - 1][j] + NEMO_BOARD[i - 1][j + 1] + NEMO_BOARD[i][j + 1];
-  }
-  return cntNemo !== 3;
-}
-
-function checkLeftBottom(i, j) {
-  let cntNemo = 0;
-  if (checkInRange(i, j - 1) && checkInRange(i + 1, j - 1) && checkInRange(i + 1, j)) {
-    cntNemo = NEMO_BOARD[i][j - 1] + NEMO_BOARD[i + 1][j - 1] + NEMO_BOARD[i + 1][j];
-  }
-  return cntNemo !== 3;
-}
-
-function checkRightBottom(i, j) {
-  let cntNemo = 0;
-  if (checkInRange(i, j + 1) && checkInRange(i + 1, j) && checkInRange(i + 1, j + 1)) {
-    cntNemo = NEMO_BOARD[i][j + 1] + NEMO_BOARD[i + 1][j] + NEMO_BOARD[i + 1][j + 1];
-  }
-  return cntNemo !== 3;
-}
-
-function combination(idx, cnt, target) {
-  if (cnt === target) {
-    // console.log(NEMO_BOARD);
     answer += 1;
     return;
   }
 
-  for (let nextIdx = idx; nextIdx < TOTAL; nextIdx++) {
-    const row = Math.floor(nextIdx / M);
-    const col = nextIdx % M;
+  const [nr, nc] = c + 1 === M ? [r + 1, 0] : [r, c + 1];
 
-    if (checkAround(row, col) && NEMO_BOARD[row][col] === 0) {
-      NEMO_BOARD[row][col] = 1;
-      combination(nextIdx + 1, cnt + 1, target);
-      NEMO_BOARD[row][col] = 0;
-    }
-  }
+  // 해당 위치에 넴모를 배치하거나 안하거나
+  NEMO_BOARD[r][c] = 1;
+  dfs(nr, nc);
+
+  NEMO_BOARD[r][c] = 0;
+  dfs(nr, nc);
 }
 
-// 놓아볼 넴모의 개수 (2개부터)
-for (let target = 2; target < TOTAL; target++) {
-  combination(0, 0, target);
-}
+const [N, M] = `1 25`.split(" ").map(Number);
+const NEMO_BOARD = Array(N)
+  .fill()
+  .map(() => Array(M).fill(0));
+let answer = 0;
 
+dfs(0, 0);
 console.log(answer);
