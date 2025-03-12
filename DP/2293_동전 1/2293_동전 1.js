7f9ca9e137347084bd84d264a885b075c95f405b
gyur1kim <qri1114@gmail.com>
2025-03-12 23:53:14 +0900

    refactor: 동전1

--- DP/2293_동전 1/2293_동전 1.js
+++ DP/2293_동전 1/2293_동전 1.js
@@ -1,20 +1,17 @@
-let [nk, ...coins] = `2 10
+const [N, K, ...coins] = `3 10
+1
 2
-5`.split('\n');
+5`
+  .split(/\s+/)
+  .map(Number);
 
-let [n, k] = nk.split(' ').map(Number);
-coins = coins.map(Number);
+const DP = Array(K + 1).fill(0);
+DP[0] = 1;
 
-let dp = new Array(k+1).fill(0);
-dp[0] = 1;
-console.log(dp);
-
-for (let i = 0; i < n; i++) {
-  // k = n 개의 동전으로 만들 수 있는 총 합
-  for (let j = coins[i]; j <= k; j++) {
-    dp[j] += dp[j - coins[i]];
+for (const coin of coins) {
+  for (let i = coin; i <= K; i++) {
+    DP[i] = DP[i] + DP[i - coin];
   }
 }
 
-console.log(dp[k])
-
+console.log(DP[K + 1]);
