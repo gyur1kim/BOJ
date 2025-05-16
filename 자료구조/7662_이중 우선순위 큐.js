ffc7fa3a4be22aa585949833ff6b375d3d04ce5d
gyur1kim <qri1114@gmail.com>
2025-05-16 16:20:41 +0900

    feat: 이중 우선순위 큐
    - 82%에서 메모리초과 발생..

---
+++ 자료구조/7662_이중 우선순위 큐.js
@@ -0,0 +1,178 @@
+class PriorityQueue {
+  constructor(isMinHeap = true) {
+    this.queue = [];
+    this.isMinHeap = isMinHeap;
+  }
+
+  enqueue(item) {
+    if (!this.isMinHeap) item *= -1;
+    this.queue.push(item);
+    this.heapifyUp();
+  }
+
+  dequeue() {
+    if (this.queue.length === 0) return null;
+
+    const item = this.queue[0];
+    const last = this.queue.pop();
+
+    if (this.queue.length > 0) {
+      this.queue[0] = last;
+      this.heapifyDown();
+    }
+
+    return this.isMinHeap ? item : item * -1;
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  getLeftChildIdx(idx) {
+    return idx * 2 + 1;
+  }
+
+  getRightChildIdx(idx) {
+    return idx * 2 + 2;
+  }
+
+  getParentIdx(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  swap(idx1, idx2) {
+    [this.queue[idx1], this.queue[idx2]] = [this.queue[idx2], this.queue[idx1]];
+  }
+
+  heapifyUp() {
+    let idx = this.queue.length - 1;
+
+    while (idx > 0) {
+      const parentIdx = this.getParentIdx(idx);
+      if (this.queue[parentIdx] > this.queue[idx]) {
+        this.swap(parentIdx, idx);
+        idx = parentIdx;
+      } else {
+        break;
+      }
+    }
+  }
+
+  heapifyDown() {
+    let idx = 0;
+    const length = this.queue.length;
+
+    while (idx < length) {
+      const leftIdx = this.getLeftChildIdx(idx);
+      const rightIdx = this.getRightChildIdx(idx);
+      let smallest = idx;
+
+      if (leftIdx < length && this.queue[smallest] > this.queue[leftIdx]) {
+        smallest = leftIdx;
+      }
+      if (rightIdx < length && this.queue[smallest] > this.queue[rightIdx]) {
+        smallest = rightIdx;
+      }
+
+      if (smallest !== idx) {
+        this.swap(idx, smallest);
+        idx = smallest;
+      } else {
+        break;
+      }
+    }
+  }
+}
+
+function solution(operations) {
+  // pq는 최소힙으로 구현됨
+  const MAX_PQ = new PriorityQueue(false); // -1을 곱해서, 큰 값이 위로 오게 할거임
+  const MIN_PQ = new PriorityQueue(true);
+  const numMap = new Map();
+
+  for (const operation of operations) {
+    let [o, val] = operation.split(" ");
+    val = +val;
+
+    switch (o) {
+      case "I":
+        MAX_PQ.enqueue(val);
+        MIN_PQ.enqueue(val);
+        numMap.set(val, (numMap.get(val) || 0) + 1);
+        break;
+      case "D":
+        // 작은 값을 빼세요
+        if (val === -1) {
+          getMin();
+        }
+        // 큰 값을 빼세요
+        else {
+          getMax();
+        }
+    }
+  }
+
+  if (numMap.size === 0) return "EMPTY";
+
+  const min = getMin();
+  const max = getMax();
+
+  if (max === null || min === null) return `${min} ${min}`;
+  return `${max} ${min}`;
+
+  function getMin() {
+    while (!MIN_PQ.isEmpty()) {
+      const min = MIN_PQ.dequeue();
+
+      if (!numMap.has(min)) continue;
+
+      const cnt = numMap.get(min);
+      if (cnt === 1) {
+        numMap.delete(min);
+      } else {
+        numMap.set(min, cnt - 1);
+      }
+      return min;
+    }
+    return null;
+  }
+
+  function getMax() {
+    while (!MAX_PQ.isEmpty()) {
+      let max = MAX_PQ.dequeue();
+
+      if (!numMap.has(max)) continue;
+
+      const cnt = numMap.get(max);
+      if (cnt === 1) {
+        numMap.delete(max);
+      } else {
+        numMap.set(max, cnt - 1);
+      }
+
+      return max;
+    }
+    return null;
+  }
+}
+
+const input = `1
+7
+I 66
+I 89
+D 1
+I 78
+D 1
+I 99
+D -1`.split("\n");
+const TC = input[0];
+let idx = 1;
+let ans = "";
+for (let tc = 0; tc < TC; tc++) {
+  let len = +input[idx];
+  const operations = input.slice(idx + 1, idx + len + 1);
+  const res = solution(operations);
+  ans += `${res}\n`;
+  idx += len + 1;
+}
+console.log(ans);
